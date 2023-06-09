72032379ed2383c47025e70ac971cba7be3f951a
sayyidpane9 <sayyidpane9@gmail.com>
2023-06-09 20:31:39 +0700

    Add README.md

--- Go-playground/go.mod/main.go
+++ Go-playground/go.mod/main.go
@@ -1,9 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func capitalize(str string) string {
+	words := strings.Fields(str) 
+	for i, word := range words {
+		words[i] = strings.Title(strings.ToLower(word))
+	}
+	return strings.Join(words, " ") // gabungkan kata-kata kembali menjadi string
+}
 
 func main() {
-	fmt.println("Hello World!")
-
-	fmt.println("Golang")
+	fmt.Println(capitalize("alterra academy")) 
+	fmt.Println(capitalize("ruMaH MAkAn"))     
 }
