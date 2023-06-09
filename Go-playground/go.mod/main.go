98e06ecca5dd9342356bed6a5cae055f23a92bb8
sayyidpane9 <sayyidpane9@gmail.com>
2023-06-09 20:35:04 +0700

    Revert "Add README.md"
    This reverts commit 72032379ed2383c47025e70ac971cba7be3f951a.

--- Go-playground/go.mod/main.go
+++ Go-playground/go.mod/main.go
@@ -1,19 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
-func capitalize(str string) string {
-	words := strings.Fields(str) 
-	for i, word := range words {
-		words[i] = strings.Title(strings.ToLower(word))
-	}
-	return strings.Join(words, " ") // gabungkan kata-kata kembali menjadi string
-}
+import "fmt"
 
 func main() {
-	fmt.Println(capitalize("alterra academy")) 
-	fmt.Println(capitalize("ruMaH MAkAn"))     
+	fmt.println("Hello World!")
+
+	fmt.println("Golang")
 }
